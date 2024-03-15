29b204728122ba8f9cafb4ae48a42945c689ba57
Veera Suominen <veera.suomine@gmail.com>
2024-03-15 12:05:55 +0200

    demo2

---
+++ Assets/Scripts/customInput.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customInput : MonoBehaviour
+{
+    public void OnMenu()
+    {
+        Debug.Log("Menu button pressed");
+    }
+}
